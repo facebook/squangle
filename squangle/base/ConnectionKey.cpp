19c088af2ebb184d0faf4cd1d009fac58aee8fcb
Adriana Liborio Fernandes Lins <lins@fb.com>
2016-10-06 19:40:42 -0700

    Move ConnectionKey to folly::Hash
    Summary: StringPiece::hash is deprecated. Update Squangle.
    
    Reviewed By: luciang
    
    Differential Revision: D3985793
    
    fbshipit-source-id: a1aced9e7c4d1aee9f009acd0cddc67867656c49

---
+++ squangle/base/ConnectionKey.cpp
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include "squangle/base/ConnectionKey.h"
+
+#include <folly/Format.h>
+#include <folly/Hash.h>
+
+namespace facebook {
+namespace common {
+namespace mysql_client {
+
+ConnectionKey::ConnectionKey(
+    folly::StringPiece sp_host,
+    int sp_port,
+    folly::StringPiece sp_db_name,
+    folly::StringPiece sp_user,
+    folly::StringPiece sp_password,
+    folly::StringPiece sp_special_tag,
+    bool sp_ignore_db_name)
+    : host(sp_host.toString()),
+      port(sp_port),
+      db_name(sp_db_name.toString()),
+      user(sp_user.toString()),
+      password(sp_password.toString()),
+      special_tag(sp_special_tag.toString()),
+      ignore_db_name(sp_ignore_db_name),
+      hash(folly::Hash()(
+          sp_host,
+          sp_port,
+          ignore_db_name ? "" : sp_db_name,
+          sp_user,
+          sp_password,
+          sp_special_tag)) {}
+
+bool ConnectionKey::operator==(const ConnectionKey& rhs) const {
+  return hash == rhs.hash && host == rhs.host && port == rhs.port &&
+      (ignore_db_name || db_name == rhs.db_name) && user == rhs.user &&
+      password == rhs.password && special_tag == rhs.special_tag;
+}
+
+std::string ConnectionKey::getDisplayString() const {
+  return folly::sformat(
+      "{} [{}] ({}@{}:{})", db_name, special_tag, user, host, port);
+}
+}
+}
+}
--- squangle/base/ConnectionKey.h
+++ squangle/base/ConnectionKey.h
@@ -11,7 +11,6 @@
 #pragma once
 
 #include <folly/String.h>
-#include <folly/Format.h>
 
 namespace facebook {
 namespace common {
@@ -41,34 +40,13 @@ class ConnectionKey {
       folly::StringPiece sp_user,
       folly::StringPiece sp_password,
       folly::StringPiece sp_special_tag = "",
-      bool sp_ignore_db_name = false)
-      : host(sp_host.toString()),
-        port(sp_port),
-        db_name(sp_db_name.toString()),
-        user(sp_user.toString()),
-        password(sp_password.toString()),
-        special_tag(sp_special_tag.toString()),
-        ignore_db_name(sp_ignore_db_name),
-        hash(folly::hash::hash_combine(
-            sp_host.hash(),
-            sp_port,
-            ignore_db_name ? 0 : sp_db_name.hash(),
-            sp_user.hash(),
-            sp_password.hash(),
-            sp_special_tag.hash())) {}
+      bool sp_ignore_db_name = false);
 
-  bool operator==(const ConnectionKey& rhs) const {
-    return hash == rhs.hash && host == rhs.host && port == rhs.port &&
-        (ignore_db_name || db_name == rhs.db_name) && user == rhs.user &&
-        password == rhs.password && special_tag == rhs.special_tag;
-  }
+  bool operator==(const ConnectionKey& rhs) const;
 
   bool operator!=(const ConnectionKey& rhs) const { return !(*this == rhs); }
 
-  std::string getDisplayString() const {
-    return folly::sformat("{} [{}] ({}@{}:{})", db_name, special_tag, user,
-                          host, port);
-  }
+  std::string getDisplayString() const;
 };
 }
 }
